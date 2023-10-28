e90a017fb4898cef8f60415def19687ef90df96c
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-10-28 12:29:52 +0200

    up

--- bin/loader.cmd
+++ bin/loader.cmd
@@ -1,15 +1,10 @@
 git status
 git pull
 
-REM write a question to update the dependencies and if the user says yes, then run update-deps.cmd
-REM if the user says no, then continue
-
 @echo off
 set /p update=Do you want to update the dependencies? (y/n)
 if %update%==y (
-    call ./update-deps.cmd
-) else (
-    echo "Skipping dependency update"
+    call update-deps.cmd
 )
 
 start ./server.cmd
