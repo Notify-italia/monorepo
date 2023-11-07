ec05c6213ab3c86ad7c1151a1971cf0faacd4860
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-11-07 17:07:46 +0100

    rimuove commenti

--- api-setup.cmd
+++ api-setup.cmd
@@ -1,14 +1,14 @@
-# Install WSL
+
 wsl --install Ubuntu
 
-# Open WSL and install npm
+
 wsl
 sudo apt update
 sudo apt install npm
 
-# Install Bun.sh
+
 sudo npm install -g bun
 
-# Close the terminal all together
+
 exit
 
