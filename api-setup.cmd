800b6e06f756cf633d2b47678346853e46d82abf
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-11-07 17:02:39 +0100

    update demo

---
+++ api-setup.cmd
@@ -0,0 +1,14 @@
+# Install WSL
+wsl --install
+
+# Open WSL and install npm
+wsl
+sudo apt update
+sudo apt install npm
+
+# Install Bun.sh
+sudo npm install -g bun
+
+# Close the terminal all together
+exit
+
