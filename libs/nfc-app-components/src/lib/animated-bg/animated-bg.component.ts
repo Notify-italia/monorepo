94aa5480ebdc1e7d5b4710035299cea7064b24a0
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-11-12 20:46:14 +0100

    modifiche animated bg

--- libs/nfc-app-components/src/lib/animated-bg/animated-bg.component.ts
+++ libs/nfc-app-components/src/lib/animated-bg/animated-bg.component.ts
@@ -9,12 +9,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./animated-bg.component.scss'],
 })
 export class AnimatedBgComponent {
-  public listItems = new Array(30).fill(0).map((i) => ({
-    left: this.random(0, 90) + '%',
-    delay: this.random(0, 20) + 's',
-  }));
+  public listItems = new Array(20).fill(0).map((i, index) => {
+    const scale = this.random(0.6, 1.4);
 
-  public random(min: number, max: number) {
-    return Math.floor(Math.random() * (max - min + 1) + min);
+    const blur = scale < 1.2 ? 0 : (scale - 1) * 10;
+
+    return {
+      left: `${index % 2 ? this.random(0, 40) : this.random(60, 90)}%`,
+      delay:
+        (index < 5 ? this.random(0, 4, index) : this.random(0, 20, index)) +
+        's',
+      scale,
+      blur,
+    };
+  });
+
+  public random(min: number, max: number, index?: number): number {
+    if (!index) {
+      return Math.random() * (max - min) + min;
+    }
+
+    //random number that scales lineary with the index but doesn't exceed the min/max values
+    return Math.random() * (max - min) + min + (index / 30) * (max - min);
   }
 }
