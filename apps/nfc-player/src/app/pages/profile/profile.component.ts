293dbbdc1b62d6ff951ab65e0a1dba50179543d3
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-11-13 01:57:06 +0100

    aggiunge public url in player

--- apps/nfc-player/src/app/pages/profile/profile.component.ts
+++ apps/nfc-player/src/app/pages/profile/profile.component.ts
@@ -5,6 +5,7 @@ import { ProfileViewComponent } from '@notify/nfc-app-components';
 import { ApiService } from '@notify/nfc-app-services';
 import { INotifyProfile } from '@notify/nfc-interfaces';
 import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment';
 
 @Component({
   selector: 'notify-profile',
@@ -15,6 +16,8 @@ import { Observable } from 'rxjs';
 })
 export class ProfileComponent {
   public profile$: Observable<INotifyProfile>;
+
+  public publicUrl = environment.publicUrl;
 
   constructor(
     private _activatedRoute: ActivatedRoute,
--- apps/nfc-player/src/environments/environment.prod.ts
+++ apps/nfc-player/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   apiUrl: 'https://exposed-api.vps.golden-italia.it',
+  publicUrl: 'https://exposed.vps.golden-italia.it',
 };
