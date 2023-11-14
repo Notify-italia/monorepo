372cc89c9b63e5b6c6e463a085b92a4137bd8d29
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-11-14 22:53:50 +0100

    todos

--- apps/nfc-agent-client/src/environments/environment.prod.ts
+++ apps/nfc-agent-client/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.9:3100/api',
-  publicUrl: 'http://192.168.1.9:4200',
+  apiUrl: 'http://172.20.10.2:3100/api',
+  publicUrl: 'http://172.20.10.2:4200',
   jwtTokenKey: 'dev.agent.token',
 };
--- apps/nfc-player/src/environments/environment.prod.ts
+++ apps/nfc-player/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://192.168.1.9:3100/api',
-  publicUrl: 'http://192.168.1.9:4200',
+  apiUrl: 'http://172.20.10.2:3100/api',
+  publicUrl: 'http://172.20.10.2:4200',
 };
