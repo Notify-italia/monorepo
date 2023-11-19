d5d7ed621ec2f56013b840b8740c83247e79a4cd
StefanoVe <stefano.vecchietti.99@gmail.com>
2023-11-19 22:18:04 +0100

    aggiorna envs

--- apps/nfc-player/src/environments/environment.prod.ts
+++ apps/nfc-player/src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   apiUrl: 'https://notify-api.vps.notifygroup.it/api',
-  publicUrl: 'https://profiles-player.vps.notifygroup.it',
+  publicUrl: 'https://profili.notifygroup.it',
   feedbackKey: 'feedback',
 };
