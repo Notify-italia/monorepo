b1d3c3ebe3e785d7fa76d4c176e280f279158c6b
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-01-23 14:48:22 +0100

    modifica loader

--- libs/ngx-components/src/lib/standalones/loading/loading.component.ts
+++ libs/ngx-components/src/lib/standalones/loading/loading.component.ts
@@ -1,6 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input, OnInit } from '@angular/core';
-import { Observable, interval, map, startWith } from 'rxjs';
+import { Component } from '@angular/core';
 import { AppTitleComponent } from '../app-title/app-title.component';
 
 @Component({
@@ -10,16 +9,4 @@ import { AppTitleComponent } from '../app-title/app-title.component';
   templateUrl: './loading.component.html',
   styleUrls: ['./loading.component.scss'],
 })
-export class LoadingComponent implements OnInit {
-  @Input() public interval = 400;
-  private _rotationFactor = 90;
-
-  public rotationDegrees$: Observable<number> | undefined;
-
-  ngOnInit(): void {
-    this.rotationDegrees$ = interval(this.interval).pipe(
-      startWith(0),
-      map((i) => i * this._rotationFactor + 1)
-    );
-  }
-}
+export class LoadingComponent {}
