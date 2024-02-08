b367ed891339b92aa9ddf8624c5c0933d1b76eb9
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-02-08 19:49:40 +0100

    marketing wallpaper e setup deploy root

--- apps/nfc-root-client/src/app/app.routes.ts
+++ apps/nfc-root-client/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: 'marketing',
+    children: [
+      {
+        path: 'wallpaper',
+        loadComponent: () =>
+          import(
+            './pages/marketing-wallpaper/marketing-wallpaper.component'
+          ).then((m) => m.MarketingWallpaperComponent),
+      },
+    ],
+  },
+];
--- libs/ngx-components/src/lib/standalones/animated-bg/animated-bg.component.ts
+++ libs/ngx-components/src/lib/standalones/animated-bg/animated-bg.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Component, Input, OnChanges, OnInit } from '@angular/core';
 
 @Component({
   selector: 'notify-animated-bg',
@@ -8,22 +8,62 @@ import { Component } from '@angular/core';
   templateUrl: './animated-bg.component.html',
   styleUrls: ['./animated-bg.component.scss'],
 })
-export class AnimatedBgComponent {
-  public listItems = new Array(20).fill(0).map((i, index) => {
-    const scale = this.random(0.6, 1.4);
+export class AnimatedBgComponent implements OnInit, OnChanges {
+  @Input() public config = {
+    blur: {
+      amount: 10,
+      threshold: 1.2,
+    },
+    scale: {
+      min: 0.6,
+      max: 1.4,
+    },
+    evenItemsOffsets: {
+      min: 0,
+      max: 40,
+    },
+    oddItemsOffsets: {
+      min: 60,
+      max: 90,
+    },
+    cards: 20,
+    delay: {
+      fastCards: {
+        items: 5,
+        min: 0,
+        max: 4,
+      },
+      items: {
+        min: 0,
+        max: 20,
+      },
+    },
+  };
 
-    const blur = scale < 1.2 ? 0 : (scale - 1) * 10;
+  public listItems: {
+    left: string;
+    delay: string;
+    scale: number;
+    blur: number;
+  }[] = [];
 
-    return {
-      left: `${index % 2 ? this.random(0, 40) : this.random(60, 90)}%`,
-      delay:
-        (index < 5 ? this.random(0, 4, index) : this.random(0, 20, index)) +
-        's',
-      scale,
-      blur,
-    };
-  });
+  public ngOnInit(): void {
+    this.listItems = this._renderPage();
+  }
 
+  /**
+   * The function generates a random number within a given range, with an optional index parameter that
+   * scales the randomness.
+   * @param {number} min - The minimum value for the random number.
+   * @param {number} max - The "max" parameter represents the maximum value that the random number can
+   * be.
+   * @param {number} [index] - The `index` parameter is an optional parameter that represents the index
+   * or position of the random number in a sequence. It is used to scale the random number linearly
+   * based on the index value.
+   * @returns a random number between the specified minimum and maximum values. If an index is
+   * provided, the returned number is scaled linearly with the index but does not exceed the minimum
+   * and maximum values.
+   */
   public random(min: number, max: number, index?: number): number {
     if (!index) {
       return Math.random() * (max - min) + min;
@@ -31,5 +71,48 @@ export class AnimatedBgComponent {
 
     //random number that scales lineary with the index but doesn't exceed the min/max values
     return Math.random() * (max - min) + min + (index / 30) * (max - min);
+  }
+
+  private _renderPage() {
+    return new Array(this.config.cards).fill(0).map((i, index) => {
+      const scale = this.random(this.config.scale.min, this.config.scale.max);
+
+      const blur =
+        scale < this.config.blur.threshold
+          ? 0
+          : (scale - 1) * this.config.blur.amount;
+
+      return {
+        left: `${
+          index % 2
+            ? this.random(
+                this.config.evenItemsOffsets.min,
+                this.config.evenItemsOffsets.max
+              )
+            : this.random(
+                this.config.oddItemsOffsets.min,
+                this.config.oddItemsOffsets.max
+              )
+        }%`,
+        delay:
+          (index < this.config.delay.fastCards.items
+            ? this.random(
+                this.config.delay.fastCards.min,
+                this.config.delay.fastCards.max,
+                index
+              )
+            : this.random(
+                this.config.delay.items.min,
+                this.config.delay.items.max,
+                index
+              )) + 's',
+        scale,
+        blur,
+      };
+    });
+  }
+
+  public ngOnChanges(): void {
+    this.listItems = this._renderPage();
   }
 }
