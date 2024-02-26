991509c412b2990c545dadb9c7702578eedbaf6d
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-02-26 18:58:25 +0100

    modifiche minori

--- apps/nfc-company-client/src/app/pages/license/license.component.ts
+++ apps/nfc-company-client/src/app/pages/license/license.component.ts
@@ -104,6 +104,6 @@ export class LicenseComponent {
   }
 
   private _contactUs(): void {
-    window.location.href = 'mailto:vendite@notifyapp.it';
+    window.location.href = `mailto:supporto@notifyapp.it?subject=Disdetta&body=Vorrei richiedere la disdetta del mio account Notify con licenza ${this.license.publicKey}. Grazie.`;
   }
 }
