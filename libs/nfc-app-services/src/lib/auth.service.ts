ac4b7e5fc5716f814e233b2df4feaf9f7fce325a
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-03-03 22:10:06 +0100

    rimuove alert

--- libs/nfc-app-services/src/lib/auth.service.ts
+++ libs/nfc-app-services/src/lib/auth.service.ts
@@ -89,7 +89,6 @@ export class AuthService {
       .pipe(
         tap((user) => this._assignToken(user)),
         catchError(() => {
-          confirm("Licenza scaduta, contattare l'amministratore.");
           this.signOut();
           return of(null);
         })
