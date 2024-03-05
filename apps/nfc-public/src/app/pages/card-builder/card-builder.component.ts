b759554126106c605add180003aceeb5b0921a1e
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-03-05 01:15:33 +0100

    NOTIFY-120 continua implementazione builder

--- apps/nfc-public/src/app/pages/card-builder/card-builder.component.ts
+++ apps/nfc-public/src/app/pages/card-builder/card-builder.component.ts
@@ -33,6 +33,89 @@ export class CardBuilderComponent {
         },
       ],
     },
+    {
+      theme: 'esagoni',
+      thumb: '/assets/cards/esagoni/thumb.png',
+      items: [
+        {
+          title: 'river',
+          path: '/assets/cards/esagoni/river.svg',
+          thumb: '/assets/cards/esagoni/river_thumb.png',
+        },
+        {
+          title: 'banana',
+          path: '/assets/cards/esagoni/banana.svg',
+          thumb: '/assets/cards/esagoni/banana_thumb.png',
+        },
+        {
+          title: 'neon',
+          path: '/assets/cards/esagoni/neon.svg',
+          thumb: '/assets/cards/esagoni/neon_thumb.png',
+        },
+      ],
+    },
+    {
+      theme: 'cubi',
+      thumb: '/assets/cards/cubi/thumb.png',
+      items: [
+        {
+          title: 'river',
+          path: '/assets/cards/cubi/river.svg',
+          thumb: '/assets/cards/cubi/river_thumb.png',
+        },
+        {
+          title: 'banana',
+          path: '/assets/cards/cubi/banana.svg',
+          thumb: '/assets/cards/cubi/banana_thumb.png',
+        },
+        {
+          title: 'neon',
+          path: '/assets/cards/cubi/neon.svg',
+          thumb: '/assets/cards/cubi/neon_thumb.png',
+        },
+      ],
+    },
+    {
+      theme: 'pastello',
+      thumb: '/assets/cards/pastello/thumb.png',
+      items: [
+        {
+          title: 'black',
+          path: '/assets/cards/pastello/black.svg',
+          thumb: '/assets/cards/pastello/black_thumb.png',
+        },
+        {
+          title: 'blue',
+          path: '/assets/cards/pastello/blue.svg',
+          thumb: '/assets/cards/pastello/blue_thumb.png',
+        },
+        {
+          title: 'grass',
+          path: '/assets/cards/pastello/grass.svg',
+          thumb: '/assets/cards/pastello/grass_thumb.png',
+        },
+        {
+          title: 'lemon',
+          path: '/assets/cards/pastello/lemon.svg',
+          thumb: '/assets/cards/pastello/lemon_thumb.png',
+        },
+        {
+          title: 'purple',
+          path: '/assets/cards/pastello/purple.svg',
+          thumb: '/assets/cards/pastello/purple_thumb.png',
+        },
+        {
+          title: 'pink',
+          path: '/assets/cards/pastello/pink.svg',
+          thumb: '/assets/cards/pastello/pink_thumb.png',
+        },
+        {
+          title: 'red',
+          path: '/assets/cards/pastello/red.svg',
+          thumb: '/assets/cards/pastello/red_thumb.png',
+        },
+      ],
+    },
   ];
 
   public selectedCard = this.cards[0].items[0];
