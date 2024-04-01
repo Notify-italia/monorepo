39bd9794b1409dfccdc07f1bcfdd8be0ecf3d22b
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-04-01 18:21:20 +0200

    fix imports

---
+++ libs/ngx-shared/src/lib/modules/modals/_qrcode/components/qrcode/qrcode.factory.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { BaseFactory } from '../../../../../constructors/base.factory';
+import { QrcodeComponent } from '../../../qrcode';
+
+@Injectable()
+export class QrcodeFactory extends BaseFactory {
+  public create(config: {
+    data: string;
+    title: string;
+    size?: number;
+    filename?: string;
+    blurBackground?: boolean;
+  }) {
+    return this._createComponent(QrcodeComponent, {
+      ...config,
+      blurBackground: config.blurBackground ?? true,
+    });
+  }
+}
