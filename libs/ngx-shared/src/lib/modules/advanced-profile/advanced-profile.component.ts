08428b88dc3ddfdd9efc8e841073357f9ab215a5
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-05-10 01:26:15 +0200

    mock available items e hierarchy

--- libs/ngx-shared/src/lib/modules/advanced-profile/advanced-profile.component.ts
+++ libs/ngx-shared/src/lib/modules/advanced-profile/advanced-profile.component.ts
@@ -1,11 +1,12 @@
-import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { LeftMenuComponent } from './parts/left-menu.component';
 
 @Component({
   selector: 'notify-advanced-profile',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, LeftMenuComponent],
   templateUrl: './advanced-profile.component.html',
-  styleUrl: './advanced-profile.component.scss',
+  styleUrl: './advanced-profile.styles.scss',
 })
 export class AdvancedProfileComponent {}
