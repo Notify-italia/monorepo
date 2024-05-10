1dff74d94ded6f5b93e7e70d483e9a0eeb607d1b
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-05-10 18:27:40 +0200

    sposta folder

--- libs/ngx-shared/src/lib/modules/advanced-profile/parts/left-menu/left-menu.component.ts
+++ libs/ngx-shared/src/lib/modules/advanced-profile/parts/left-menu/left-menu.component.ts
@@ -1,13 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { AvailableItemsComponent } from '../components/available-items/available-items.component';
-import { HierarchyComponent } from '../components/hierarchy/hierarchy.component';
+import { AvailableItemsComponent } from '../../components/available-items/available-items.component';
+import { HierarchyComponent } from '../../components/hierarchy/hierarchy.component';
 
 @Component({
   selector: 'notify-left-menu',
   standalone: true,
   imports: [CommonModule, AvailableItemsComponent, HierarchyComponent],
   templateUrl: './left-menu.component.html',
-  styleUrls: ['./left-menu.component.scss', '../advanced-profile.styles.scss'],
+  styleUrls: [
+    './left-menu.component.scss',
+    '../../advanced-profile.styles.scss',
+  ],
 })
 export class LeftMenuComponent {}
