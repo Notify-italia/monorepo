e8a78f53f6e61d10ea51894c2dcfeabaf807abd2
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-05-11 00:02:37 +0200

    adatta save indicator e share per il nuovo editor

--- libs/ngx-shared/src/lib/modules/advanced-profile/advanced-profile.component.ts
+++ libs/ngx-shared/src/lib/modules/advanced-profile/advanced-profile.component.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { LeftMenuComponent } from './parts/left-menu/left-menu.component';
+import { LeftPanelComponent } from './parts/left-panel/left-panel.component';
+import { RightPanelComponent } from './parts/right-panel/right-panel.component';
 
 @Component({
   selector: 'notify-advanced-profile',
   standalone: true,
-  imports: [CommonModule, LeftMenuComponent],
+  imports: [CommonModule, LeftPanelComponent, RightPanelComponent],
   templateUrl: './advanced-profile.component.html',
   styleUrl: './advanced-profile.styles.scss',
 })
