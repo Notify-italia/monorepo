e633e3e1abbf028381cf2b37fea6305cb69c4b1d
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-05-28 00:02:47 +0200

    continua responsiveness

--- libs/ngx-shared/src/lib/modules/advanced-profile/parts/right-panel/right-panel.component.ts
+++ libs/ngx-shared/src/lib/modules/advanced-profile/parts/right-panel/right-panel.component.ts
@@ -30,30 +30,11 @@ export class RightPanelComponent {
     controlsFromObject<INotifyProfile['advancedProfile']>
   >;
   @Input({ required: true }) selectedHierarchyItem = 'background';
+  @Input() shareConfig!: INotifyShareItemConfig;
 
   @Output() forceSave = new EventEmitter<void>();
   @Output() closePanel = new EventEmitter<void>();
   @Output() removeItem = new EventEmitter<string>();
 
   public TEMP_today = new Date();
-
-  public get shareConfig(): INotifyShareItemConfig {
-    return {
-      type: 'profile',
-      qrcode: {
-        fileName: `profile.png`,
-      },
-      nfc: {
-        items: [
-          {
-            value: this.profile?._id,
-            label: 'Conferma',
-          },
-        ],
-      },
-      baseUrl: this.environment['profilesUrl'] as string,
-      isInModal: false,
-      id: this.profile?._id,
-    };
-  }
 }
