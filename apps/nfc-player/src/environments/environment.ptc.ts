9ae910d004190381288ec864b233cd1eeacf51bd
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-06-01 01:07:37 +0200

    ptc environment

---
+++ apps/nfc-player/src/environments/environment.ptc.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  production: true,
+  apiUrl: 'https://ptc-notify-api.vps.notifyapp.it/api',
+  publicUrl: 'https://ptc-profiles-player.vps.notifyapp.it',
+  feedbackKey: 'feedback',
+  socketUrl: 'https://ptc-notify-api.vps.notifyapp.it',
+  socketIdKey: 'prod.socketId',
+};
