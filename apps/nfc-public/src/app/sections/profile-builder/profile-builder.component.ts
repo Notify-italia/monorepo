b22885698a40a604399e85125f6ed6f4b443f33c
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-06-18 10:34:55 +0200

    continua profile builder

--- apps/nfc-public/src/app/sections/profile-builder/profile-builder.component.ts
+++ apps/nfc-public/src/app/sections/profile-builder/profile-builder.component.ts
@@ -1,6 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import {
   EnumNotifyAPAlign,
   EnumNotifyAPBackgroundTypes,
@@ -13,7 +18,11 @@ import {
   INotifyAPAvatarItem,
   INotifyProfile,
 } from '@notify/interfaces';
-import { ProfileViewComponent, TailwindFormsModule } from '@notify/ngx-shared';
+import {
+  ProfileViewComponent,
+  TailwindFormsModule,
+  UploadComponent,
+} from '@notify/ngx-shared';
 
 @Component({
   selector: 'notify-profile-builder',
@@ -23,6 +32,7 @@ import { ProfileViewComponent, TailwindFormsModule } from '@notify/ngx-shared';
     ProfileViewComponent,
     ReactiveFormsModule,
     TailwindFormsModule,
+    UploadComponent,
   ],
   templateUrl: './profile-builder.component.html',
   styles: `notify-tailwind-input {
@@ -30,25 +40,38 @@ import { ProfileViewComponent, TailwindFormsModule } from '@notify/ngx-shared';
   }
   
   .style-button {
-    @applay p-4 rounded-md shadow-md bg-[#038DE2] text-white;
+    @apply p-4 rounded-md shadow-md bg-[#038DE2] text-white w-48 h-16 hover:brightness-125 hover:scale-105 active:scale-95 disabled:pointer-events-none disabled:opacity-50 hover:h-48 hover:rounded-2xl;
   }
   `,
 })
 export class ProfileBuilderComponent {
   public form = new FormGroup({
-    name: new FormControl(''),
-    email: new FormControl(''),
-    phone: new FormControl(''),
-    description: new FormControl(''),
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', Validators.required),
+    phone: new FormControl('', Validators.required),
+    description: new FormControl('', Validators.required),
+    image: new FormControl('', Validators.required),
+    type: new FormControl('corporate'),
   });
 
+  public showProfile = false;
+
   public get currentTemplate() {
-    return this.chillProfile;
+    switch (this.form.value.type) {
+      case 'corporate':
+        return this._prepareTemplate(PROFILE_TEMPLATES_CORPORATE);
+      case 'chill':
+        return this._prepareTemplate(PROFILE_TEMPLATES_CHILL);
+      default:
+        return this._prepareTemplate(PROFILE_TEMPLATES_CORPORATE);
+    }
   }
 
-  public get chillProfile() {
-    const template = PROFILE_TEMPLATES_CHILL;
+  public onSubmit() {
+    this.showProfile = true;
+  }
 
+  private _prepareTemplate(template: INotifyProfile) {
     if (!template.advancedProfile) {
       return template;
     }
@@ -63,6 +86,10 @@ export class ProfileBuilderComponent {
     (
       template.advancedProfile.items[avatarIndex] as INotifyAPAvatarItem
     ).description = this.form.value.description || '';
+
+    (
+      template.advancedProfile.items[avatarIndex] as INotifyAPAvatarItem
+    ).imgSrc = this.form.value.image || '';
 
     return template;
   }
@@ -151,7 +178,7 @@ const PROFILE_TEMPLATES_CHILL: INotifyProfile = {
         },
         direction: EnumNotifyAPDirections.Vertical,
         label: '',
-        sublabel: 'Co-Founder | SMM',
+        sublabel: 'Web Developer Freelancer',
         useRoleSubLabel: false,
         description: '"Whatever happens, happens"',
         imgSrc:
@@ -308,4 +335,278 @@ const PROFILE_TEMPLATES_CHILL: INotifyProfile = {
   reviewRedirect: null,
   redirectUrl: null,
   piva: null,
+};
+
+const PROFILE_TEMPLATES_CORPORATE: any = {
+  _id: '65eedfa9ac3a0d7b566667de',
+  name: 'Marco',
+  surname: 'De Luca',
+  email: 'marco.deluca@squarehead.it',
+  phoneNumber: '321456987',
+  bio: 'Presso Squarehead Italia',
+  avatar:
+    'https://s3-api.vps.notifyapp.it/notify-api/profiles/65eedfa9ac3a0d7b566667de/avatar.webp',
+  role: "Responsabile del Design e dell'Innovazione",
+  config: {
+    whatsappEnabled: true,
+    phoneCallEnabled: true,
+    emailEnabled: true,
+    avatarMask: 'squircle',
+    smsEnabled: false,
+    redirectEnabled: false,
+    feedbackEnabled: false,
+  },
+  type: 'agent',
+  owner: {
+    $oid: '65eedfa9ac3a0d7b566667dd',
+  },
+  address: null,
+  reviewRedirect: null,
+  redirectUrl: null,
+  colors: {
+    background: ['#3B82F6', '#8B8B8B'],
+    elements: '#FFFFFF',
+    useCompanyColors: false,
+  },
+  customFields: [
+    {
+      iconName: 'linkedin',
+      value: 'li',
+      _id: {
+        $oid: '66709288ac39d3942c5eace1',
+      },
+    },
+    {
+      iconName: 'deviantart',
+      value: 'dsf',
+      _id: {
+        $oid: '66709288ac39d3942c5eace2',
+      },
+    },
+    {
+      iconName: 'facebook',
+      value: 'li',
+      _id: {
+        $oid: '66709288ac39d3942c5eace3',
+      },
+    },
+    {
+      iconName: 'googlecalendar',
+      value: 'squarehead.it',
+      _id: {
+        $oid: '66709288ac39d3942c5eace4',
+      },
+    },
+  ],
+  createdAt: {
+    $date: '2024-03-11T10:40:41.991Z',
+  },
+  updatedAt: {
+    $date: '2024-06-17T19:59:38.405Z',
+  },
+  noteOptions: {
+    showTitle: true,
+  },
+  advancedProfile: {
+    enabled: true,
+    items: [
+      {
+        type: 'links',
+        visible: true,
+        title: '',
+        showTitle: true,
+        textConfig: {
+          enabled: false,
+          font: 'bitter',
+          fontSize: 20,
+          textColor: '#ffffff',
+        },
+        style: 'filled',
+        direction: 'vertical',
+        openInNotify: false,
+        items: [
+          {
+            icon: 'googlecalendar',
+            url: '',
+            caption: '15 Minute Meeting',
+            visible: true,
+          },
+        ],
+        _id: '66709326734bedcfd016c087',
+      },
+      {
+        type: 'links',
+        visible: true,
+        title: '',
+        showTitle: true,
+        _id: '6670928bac39d3942c5eacfd',
+        textConfig: {
+          enabled: false,
+          font: 'poppins',
+          fontSize: 17.5,
+          textColor: '#ffffff',
+        },
+        direction: 'vertical',
+        style: 'outlined',
+        items: [
+          {
+            icon: 'linkedin',
+            caption: 'Colleghiamoci',
+            url: 'li',
+            visible: true,
+          },
+          {
+            icon: 'gmail',
+            url: '',
+            caption: "Inviami un' email",
+            visible: false,
+          },
+        ],
+        openInNotify: false,
+      },
+      {
+        type: 'divider',
+        visible: true,
+        title: '',
+        showTitle: true,
+        textConfig: {
+          enabled: false,
+          font: 'poppins',
+          fontSize: 16,
+          textColor: '#ffffff',
+        },
+        _id: '66709321734bedcfd016c086',
+      },
+      {
+        type: 'avatar',
+        visible: true,
+        title: '',
+        showTitle: true,
+        _id: '6670928bac39d3942c5eacfa',
+        textConfig: {
+          enabled: false,
+          font: 'poppins',
+          fontSize: '18',
+          textColor: '',
+        },
+        direction: 'vertical',
+        label: 'Marco De Luca',
+        sublabel: "Responsabile del Design e dell'Innovazione",
+        useRoleSubLabel: true,
+        description: 'Presso Squarehead Italia',
+        imgSrc:
+          'https://s3-api.vps.notifyapp.it/notify-api/profiles/65eedfa9ac3a0d7b566667de/avatar.webp',
+        imgMask: 'squircle',
+        ownerImgCorner: 'bottom-right',
+        align: 'center',
+        imgSize: 100,
+        imgFit: 'cover',
+      },
+      {
+        type: 'contacts',
+        visible: true,
+        title: '',
+        showTitle: true,
+        _id: '6670928bac39d3942c5eacfc',
+        textConfig: {
+          enabled: true,
+          font: 'poppins',
+          fontSize: 24.5,
+          textColor: '',
+        },
+        direction: 'horizontal',
+        style: 'text',
+        items: [
+          {
+            icon: 'phone',
+            caption: 'Telefono',
+            url: '321456987',
+            visible: true,
+          },
+          {
+            icon: 'mail',
+            caption: 'Email',
+            url: 'marco.deluca@squarehead.it',
+            visible: true,
+          },
+          {
+            icon: 'chat',
+            caption: 'Messaggio',
+            url: '',
+            visible: true,
+          },
+        ],
+      },
+      {
+        type: 'divider',
+        visible: false,
+        title: '',
+        showTitle: true,
+        textConfig: {
+          enabled: false,
+          font: 'poppins',
+          fontSize: 16,
+          textColor: '#ffffff',
+        },
+        _id: '667092f8734bedcfd016c085',
+      },
+      {
+        type: 'place',
+        visible: true,
+        title: 'Passa a trovarci...',
+        showTitle: true,
+        textConfig: {
+          enabled: false,
+          font: 'poppins',
+          fontSize: 16,
+          textColor: '#ffffff',
+        },
+        address: 'Via Tirreno',
+        civicNumber: '20',
+        city: 'Prato',
+        companyName: 'Offcenter SRL',
+        showStreetName: true,
+        zoom: 12,
+        _id: '667092b1734bedcfd016c084',
+      },
+    ],
+    pageSettings: {
+      backgroundType: 'gradient',
+      imgSrc: '',
+      fill: '',
+      gradient: {
+        direction: 'vertical',
+        colors: [
+          {
+            value: '#292929',
+          },
+          {
+            value: '#000000',
+          },
+        ],
+      },
+      topPadding: 0,
+      textColor: '#e6e6e6',
+      align: 'flex-start',
+      padding: 0.5,
+      verticalSpacing: 0.5,
+      font: 'ebGaramond',
+      fontSize: '19',
+      redirectUrl: '',
+      hideContactSave: false,
+      backgroundBlur: 0,
+      backgroundBrightness: 100,
+      useCompanyTheme: false,
+      contactOverrides: {
+        name: '',
+      },
+    },
+    requiredItems: {
+      feedback: '',
+      avatar: '6670928bac39d3942c5eacfa',
+    },
+    _id: {
+      $oid: '6670928bac39d3942c5eacfe',
+    },
+  },
 };
