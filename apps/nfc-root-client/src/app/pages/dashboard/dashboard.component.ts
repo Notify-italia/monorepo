b775987ac5dc76225f1ecd4625d65a568d6d2c72
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-06-20 20:20:04 +0200

    migliora dashboard backoffice

--- apps/nfc-root-client/src/app/pages/dashboard/dashboard.component.ts
+++ apps/nfc-root-client/src/app/pages/dashboard/dashboard.component.ts
@@ -2,17 +2,28 @@ import { CommonModule } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import {
   LoadingComponent,
+  PageHeaderComponent,
   RootService,
   WidgetCounterComponent,
 } from '@notify/ngx-shared';
+import { delay, of, repeat, switchMap } from 'rxjs';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, WidgetCounterComponent, LoadingComponent],
+  imports: [
+    CommonModule,
+    WidgetCounterComponent,
+    LoadingComponent,
+    PageHeaderComponent,
+  ],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss',
 })
 export class DashboardComponent {
   private _rootService = inject(RootService);
-  public dashboard$ = this._rootService.getDashboard();
+  public dashboard$ = of(null).pipe(
+    switchMap(() => this._rootService.getDashboard()),
+    delay(1000),
+    repeat()
+  );
 }
