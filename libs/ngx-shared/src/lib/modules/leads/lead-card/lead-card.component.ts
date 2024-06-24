799871f5cc6c31e905aa6486a1161f255451dce0
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-06-24 13:00:53 +0200

    conclude cards

--- libs/ngx-shared/src/lib/modules/leads/lead-card/lead-card.component.ts
+++ libs/ngx-shared/src/lib/modules/leads/lead-card/lead-card.component.ts
@@ -1,15 +1,18 @@
 import { CommonModule } from '@angular/common';
-import { Component, Input } from '@angular/core';
+import { Component, Input, inject } from '@angular/core';
 import { INotifyLead } from '@notify/interfaces';
+import { UtilsService } from '../../../services';
 
 @Component({
   selector: 'notify-lead-card',
   standalone: true,
   imports: [CommonModule],
+  providers: [UtilsService],
   templateUrl: './lead-card.component.html',
   styleUrl: './lead-card.component.scss',
 })
 export class LeadCardComponent {
+  private _utilsSerivce = inject(UtilsService);
   @Input({ required: true }) lead!: INotifyLead;
 
   public get mainAlias(): string {
@@ -27,5 +30,25 @@ export class LeadCardComponent {
 
   public get isPerson() {
     return !!(this.lead.name?.length || this.lead.surname?.length);
+  }
+
+  public get colors() {
+    const color = this.lead.color || '#5845b5';
+    return {
+      color: this._utilsSerivce.getContrastingColor(color),
+      'background-color': color,
+    };
+  }
+
+  public get mainEmail() {
+    return this.lead.emails?.[0];
+  }
+
+  public get mainPhoneNumber() {
+    return this.lead.phoneNumbers?.[0];
+  }
+
+  public executeCardAction(value: string, protocol: 'mailto' | 'tel') {
+    window.location.href = `${protocol}:${value}`;
   }
 }
