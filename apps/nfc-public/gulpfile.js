bf50bc22e3430d5dd88bead5617ff5f690a44684
StefanoVe <stefano.vecchietti.99@gmail.com>
2024-10-30 19:36:52 +0100

    ottimizzazioni bundle

--- apps/nfc-public/gulpfile.js
+++ apps/nfc-public/gulpfile.js
@@ -2,14 +2,26 @@ const gulp = require('gulp');
 const uglify = require('gulp-uglify');
 const cleanCSS = require('gulp-clean-css');
 const pump = require('pump');
+const fs = require("fs");
+const { join } = require("path");
+const { exec, spawn } = require('child_process');
 
-const basePath = 'dist/apps/nfc-public';
+
+
+function walk(dir) {
+  return fs.readdirSync(dir, { withFileTypes: true }).flatMap((file) => file.isDirectory() ? walk(join(dir, file.name)) : join(dir, file.name))
+}
+
+
+const basePath = process.cwd().replace('apps/nfc-public', 'dist/apps/nfc-public');
 
 gulp.task('compress-js', function (cb) {
   pump([gulp.src([`${basePath}**/*.js`]), uglify(), gulp.dest('dist')], cb);
 });
 
 gulp.task('minify-css', () => {
+
+
   return gulp
     .src([`${basePath}**/*.css`])
     .pipe(cleanCSS({ compatibility: 'ie10' }))
